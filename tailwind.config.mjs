b0e777f67c707973985621cabc8ef50b511c753b
marcgonzaleztarrio <marcgonzaleztarrio@gmail.com>
2024-08-29 20:34:29 +0200

    a qui ens dirigim primeras imatges

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			backgroundImage: {
-        'accent-gradient': 'linear-gradient(45deg, rgb(136, 58, 234), rgb(224, 204, 250) 30%, white 60%)',
-      },
-		},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "hero-qui-som": "url('/imagesProba/heroQuiSom.png')",
+                "multitud-hero": "url('/imagesProba/multitudHero.png')",
+                "nenas-hero": "url('/imagesProba/nenasHero.png')",
+            },
+        },
+    },
+    plugins: [],
+};
