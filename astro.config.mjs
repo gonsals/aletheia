5c8dcdfe81fa298de5cbf4e6ee71037786ddf846
marcgonzaleztarrio <marcgonzaleztarrio@gmail.com>
2024-09-01 01:58:27 +0200

    responsive nosaltres and cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    integrations: [
+        tailwind(),
+        icon({
+            include: {
+                // Include only three `mdi` icons in the bundle
+                mdi: ["account", "account-plus", "account-minus"],
+                // Include all `uis` icons
+                uis: ["*"],
+            },
+        }),
+    ],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
+            fontFamily: {
+                "instrument-serif": "'Instrument Serif', serif",
+            },
             backgroundImage: {
                 "hero-qui-som": "url('/imagesProba/heroQuiSom.png')",
                 "multitud-hero": "url('/imagesProba/multitudHero.png')",
